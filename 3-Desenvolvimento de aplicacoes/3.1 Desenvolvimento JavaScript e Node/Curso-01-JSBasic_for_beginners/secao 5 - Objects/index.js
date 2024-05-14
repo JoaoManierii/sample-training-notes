f67c81cde9dda573423452e5aa1a8e808d167e92
Joao Otavio Manieri <jotamanieri@gmail.com>
2024-05-14 10:18:33 -0300

    Update index.js in secao 05, add exercises in curso de JavaScript 01

--- 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-01-JSBasic_for_beginners/secao 5 - Objects/index.js
+++ 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-01-JSBasic_for_beginners/secao 5 - Objects/index.js
@@ -76,3 +76,153 @@ function increase(obj) {
 increase(obj);
 console.log(obj);
 //////////////////////////Enumerating Properties of an Object/////////////////////////////////////////////////////////////////////////
+const circle5 = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+    }
+};
+
+for (let key in circle5)
+    console.log(key, circle5[key]);
+
+for (let key of Object.keys(circle5))
+    console.log(key);
+//////////////////////////Cloning an Object /////////////////////////////////////////////////////////////////////////
+
+const circle6 = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+    }
+};
+
+const another2 = Object.assign({}, circle6);
+console.log(another2);
+
+//////////////////////////Garbage Collection//////////////////////////////////////////////////////////////////////////////////////////
+
+let circle7 = {
+    radius: 1
+};
+
+//////////////////////////Math//////////////////////////////////////////////////////////////////////////////////////////
+console.log(Math.random());
+console.log(Math.round(1.9));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+//////////////////////////String//////////////////////////////////////////////////////////////////////////////////////////
+const message = 'This is my first message';
+console.log(message.startsWith('This'));
+console.log(message.endsWith('ge'));
+console.log(message.includes('my'));
+console.log(message.indexOf('my'));
+console.log(message.replace('first', 'second'));
+console.log(message.toUpperCase());
+console.log(message.toLowerCase());
+console.log(message.trim());
+console.log(message.split(' '));
+//////////////////////////Template Literals//////////////////////////////////////////////////////////////////////////////////////////
+const name = 'Juliana';
+const message2 = `Olá ${name} ${2 + 3},
+Obrigado por se juntar a minha newsletter.
+Abracos,
+Joao`;
+console.log(message2);
+//////////////////////////Date//////////////////////////////////////////////////////////////////////////////////////////
+const now = new Date();
+const date1 = new Date('May 14 2024 09:10');
+const date2 = new Date(2024, 5, 14, 9);
+now.setFullYear(2023);
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toISOString());
+//////////////////////////EXERCISES 1//////////////////////////////////////////////////////////////////////////////////////////
+// street
+// city
+// zipCode
+// showAddress(address)
+
+let address = {
+    street: 'Rua Antonio Rodrigues Serralha',
+    city: 'Uberlandia',
+    zipCode: '38408-036'
+};
+
+function showAddress(address) {
+    for (let key in address)
+        console.log(key, address[key]);
+}
+showAddress(address);
+//////////////////////////EXERCISES 2//////////////////////////////////////////////////////////////////////////////////////////
+
+let address2 = createAddress('Rua Antonio Rodrigues Serralha', 'Uberlandia', '38408-036');
+
+function createAddress(street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode
+    };
+}
+function Address (street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+}
+//////////////////////////EXERCISES 3//////////////////////////////////////////////////////////////////////////////////////////
+function areEqual(address1, address2) {
+    return address1.street === address2.street &&
+        address1.city === address2.city &&
+        address1.zipCode === address2.zipCode;
+}
+
+function areSame(address1, address2) {
+    return address1 === address2;
+}
+//////////////////////////EXERCISES 4//////////////////////////////////////////////////////////////////////////////////////////
+// title
+// body
+// author
+// views
+// comments
+//  (author, body)
+// isLive
+
+let post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 10,
+    comments: [
+        
+        { author: 'a', body: 'b' },
+        { author: 'c', body: 'd' },
+    ],     
+    isLive: true
+};
+console.log(post);
+//////////////////////////EXERCISES 5//////////////////////////////////////////////////////////////////////////////////////////
+let post2 = new Post('a', 'b', 'c');
+
+function Post(title, body, author) {
+    this.title = title;
+    this.body = body;
+    this.author = author;
+    this.views = 0;
+    this.comments = [];
+    this.isLive = false;
+}
+//////////////////////////EXERCISES 6//////////////////////////////////////////////////////////////////////////////////////////
+let priceRanges = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+];
+
+let restaurants = [
+    { averagePerPerson: 5 }
+];
+
+console.log(priceRanges);
+    
