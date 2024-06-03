85650ca91e2163df41a1c6f5b426cb2e35c66ad2
Joao Otavio Manieri <jotamanieri@gmail.com>
2024-06-03 10:02:09 -0300

    🎉 Commit inicial, adcionado todas as dependencias e pastas

---
+++ 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-02-Formacao_Node.js/secao 32/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log('Servidor rodando na porta 3000');
+});
