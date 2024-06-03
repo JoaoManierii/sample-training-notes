f8850797aa854d7c82b8c677e352d09c37231e70
Joao Otavio Manieri <jotamanieri@gmail.com>
2024-06-03 10:17:55 -0300

    ✨ feat: Upload de arquivos com nome, extensao e data
    🧪 test: Upload de arquivos de teste

--- 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-02-Formacao_Node.js/secao 32/index.js
+++ 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-02-Formacao_Node.js/secao 32/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const multer = require('multer');
+const path = require('path');
 
 
 app.set('view engine', 'ejs');
 
-const upload = multer({ dest: "uploads/" });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
 
 app.get('/', (req, res) => {
     res.render('index');
