09e1e8dc3c2f0547cb0d31f18d18ad3ea2808952
Joao Otavio Manieri <jotamanieri@gmail.com>
2024-06-03 10:11:12 -0300

    ✨ feat: Rota d upload e instancia do middleware multer

--- 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-02-Formacao_Node.js/secao 32/index.js
+++ 3-Desenvolvimento de aplicacoes/3.1 Desenvolvimento JavaScript e Node/Curso-02-Formacao_Node.js/secao 32/index.js
@@ -5,8 +5,14 @@ const multer = require('multer');
 
 app.set('view engine', 'ejs');
 
+const upload = multer({ dest: "uploads/" });
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('Arquivo recebido com sucesso');
 });
 
 app.listen(8080, () => {
