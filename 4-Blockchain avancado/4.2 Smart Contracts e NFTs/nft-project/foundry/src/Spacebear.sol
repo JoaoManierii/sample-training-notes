b1954ee24fd630b62342b36d49968f3baddf79f1
Joao Otavio Manieri <jotamanieri@gmail.com>
2024-08-26 10:16:34 -0300

    🧱 ci: Projeto de NFT com Foundry

---
+++ 4-Blockchain avancado/4.2 Smart Contracts e NFTs/nft-project/foundry/src/Spacebear.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "forge-std/console.sol";
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Spacebear is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("Spacebear", "SBR") Ownable(msg.sender) {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter += 1;
+        _safeMint(to, tokenId);
+    }
+
+    function buyToken() public payable {
+        uint256 tokenId = _tokenIdCounter;
+        console.log("got here", tokenId, msg.value);
+        require(msg.value == (tokenId + 1) * 0.1 ether, "Not enough funds sent");
+
+        _tokenIdCounter += 1;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    // If `_burn` needs to be overridden, ensure it is marked `virtual` in the base contract.
+    // function _burn(uint256 tokenId) internal override(ERC721) {
+    //     super._burn(tokenId);
+    // }
+
+    function tokenURI(uint256 tokenId)
+        public
+        pure
+        override(ERC721)
+        returns (string memory)
+    {
+        return string(abi.encodePacked(_baseURI(), "spacebear_", Strings.toString(tokenId+ 1), ".json"));
+    }
+}
+ 
